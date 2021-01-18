96f954e7894cf5f6f8d394439df0ee49555a5672
thibault vanweydeveld <thibault.vanweydeveld@hotmail.com>
2021-01-18 14:49:32 +0800

    size fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,9 +31,9 @@ function Header(){
                 </div>
                 <ul className="mi-header-menu">
                     <li><NavLink exact to="/"><span>Home</span></NavLink></li>
-                    <li><NavLink to="/about"><span>About Me</span></NavLink></li>
+                    <li><NavLink to="/about"><span>Biographies</span></NavLink></li>
                     <li><NavLink to="/resume"><span>Price List &amp; Reviews</span></NavLink></li>
-                    <li><NavLink to="/portfolios"><span>Furry Friends</span></NavLink></li>
+                    <li><NavLink to="/portfolios"><span>My Furry Friends</span></NavLink></li>
                     <li><NavLink to="/contact"><span>Contact Me</span></NavLink></li>
                 </ul>
                 <p className="mi-header-copyright">&copy; {new Date().getFullYear()} <b><a rel="noopener noreferrer" target="_blank" href="">Honey Photography</a></b></p>
